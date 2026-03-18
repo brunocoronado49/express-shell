44945201a9c052ae83c901e0ae97e5e97efafbc9
brunocoronado49 <francorangelcoronado@gmail.com>
2026-03-18 10:27:16 -0600

    Initial commit: Base configuration and first route

---
+++ src/presentation/server.ts
@@ -0,0 +1,45 @@
+import express, { Router } from 'express';
+import path from 'node:path';
+
+interface Options {
+  port: number;
+  routes: Router;
+  publicPath?: string;
+}
+
+export class Server {
+  public readonly app = express();
+  private serverListener?: any;
+  private readonly port: number;
+  private readonly publicPath: string;
+  private readonly routes: Router;
+
+  constructor(options: Options) {
+    const { port, routes, publicPath = 'public' } = options;
+    this.port = port;
+    this.routes = routes;
+    this.publicPath = publicPath;
+  }
+
+  async start() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
+    this.app.use(express.static(this.publicPath));
+
+    this.app.use(this.routes);
+
+    this.app.get('/', (req, res) => {
+      const indexPath = path.join(__dirname + `../../${this.publicPath}/index.html`);
+      res.sendFile(indexPath);
+    });
+
+    this.serverListener = this.app.listen(this.port, () => {
+      console.log(`Server running on port: ${this.port}`);
+    });
+  }
+
+  public close() {
+    this.serverListener?.close();
+  }
+}
